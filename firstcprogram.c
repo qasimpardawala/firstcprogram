b3beb917098c46b2b246d45f8f7fa64a77465e5a
qasimpardawala <47812160+qasimpardawala@users.noreply.github.com>
2019-10-30 22:00:55 +0530

    Create firstcprogram.c

---
+++ firstcprogram.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{ int n,j,i;int arr[n];
+printf("Enter n value");
+scanf("%d ",n);
+for (i=0;i<n;i++) //user input
+{scanf("%d ",arr[i])
+}
+for(j=n-1,i=0;j<0,i<n;j--,i++)
+{ arr[j]=arr[i]
+}
+for (j=0;j<n;j++) //print array
+{printf(arr[j])
+}
+ return 0;}
