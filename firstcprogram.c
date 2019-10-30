0fab861040b2c3cc31fb97d18bbe5cce663f1033
qasimpardawala <47812160+qasimpardawala@users.noreply.github.com>
2019-10-30 22:13:59 +0530

    Update firstcprogram.c

--- firstcprogram.c
+++ firstcprogram.c
@@ -2,9 +2,9 @@
 int main()
 { int n=0; int j,i;int arr[int n];
 printf("Enter n value");
-scanf("%d ",n);
+scanf("%d ",&n);
 for (i=0;i<n;i++) //user input
-{scanf("%d ",arr[i])
+{scanf("%d ",&arr[i])
 }
 for(j=n-1,i=0;j<0,i<n;j--,i++)
 { arr[j]=arr[i]
